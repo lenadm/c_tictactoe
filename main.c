5341d41bc0f6cbd6cd31cacc7e51c390aeac40d7
lena <lenarichards2234@gmail.com>
2024-01-12 10:06:14 +0000

    setup get_user_input() function and expanded main to encompass partial game loop

--- main.c
+++ main.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define LENGTH 9
 
 void print_board(char *board);
+int get_user_input(char *gameboard);
+int get_ai_move(char *board);
 char *setup_gameboard(char *gameboard);
 
 int main(void) {
 	char *gameboard = malloc(LENGTH);
+	int counter = 0;
 
 	setup_gameboard(gameboard);
 	print_board(gameboard);
+	while(true) {
+		if (counter % 2 == 0) {
+			int move = get_user_input(gameboard);
+			gameboard[move - 1] = 'x';
+		}
+		if (counter % 2 == 1) {
+			int move = get_ai_move(gameboard);
+			gameboard[move - 1] = 'o';
+		}
+		print_board(gameboard);
+	}
 }
 
+	
+int get_user_input(char *gameboard) {
+	int input;
+	while(true) {
+		printf("which squaure would you like to place?");
+		scanf("%i", &input);
+		if(gameboard[input] != ('x' | 'o')) {
+			return input;
+		}
+	}
+}
 char *setup_gameboard(char *gameboard) {
 	for(char i = '1'; i <= '9'; i++) {
 		gameboard[i - 49] = i;
